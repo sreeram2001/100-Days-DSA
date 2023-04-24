d2722ad9da530ae0c3a667384da9fe71f6c781e5
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2023-04-24 21:21:07 +0530

    Remove nth from end in LinkedList - Two pointer method
    Given the head of a linked list, remove the nth node from the end of the list and return its head.

---
+++ Day12/remove_nth_node_linkedlist.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        //two pointer approach
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        for(int i=0;i<n;i++)
+        {
+            fast = fast->next;
+        }
+
+        if(!fast)
+        {
+            return head->next;
+        }
+
+        while(fast->next)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        //to remove the node, point next node to next to next node
+        slow->next = slow->next->next;
+        return head;
+    }
+};
