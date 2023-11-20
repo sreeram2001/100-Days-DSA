10b11281e7120db1ee8b3df1dab0edd0dfd03191
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2023-11-20 23:11:09 +0530

    Given an integer array nums where the elements are sorted in ascending order, convert it to a  height-balanced  binary search tree.

---
+++ Day68/Convert_array_to_heightbal_BST.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insert(int l, int r, vector<int> nums)
+    {
+        if(l > r) return NULL;
+        int mid = (l+r)/2;
+
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = insert(l, mid-1, nums);
+        root->right = insert(mid+1,r,nums);
+        return root;
+    }
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return insert(0, nums.size()-1, nums);
+    }
+};
