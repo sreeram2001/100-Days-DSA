88673b58ba42ad6ab2dcd22382e063d115e37f6a
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2023-12-31 23:44:06 +0530

    Directed Graphs Representation

---
+++ Day78/directed_graphs.cpp
@@ -0,0 +1,39 @@
+
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cout<<"Enter nodes :";
+    cin>>n;
+    cout<<"Enter number of EDges : ";
+    cin>>m;
+    vector<int> adj[n+1];
+    
+    cout<<"Enter nodes connections";
+    for(int i=0;i<m;i++)
+    {
+        int u, v;
+        cin>>u>>v;
+        
+        //directed graphs
+        adj[u].push_back(v);
+    }
+    
+    //print graph connections
+    for(int i=0;i<n+1;i++)
+    {
+        cout<<i<<"->";
+        for(int j=0;j<adj[i].size();j++)
+        {
+            cout<<adj[i][j]<<" ";
+        }
+        
+        cout<<" "<<endl;
+    }
+
+    return 0;
+}
